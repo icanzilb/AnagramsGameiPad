af4d5e35fee70df1708b5309ed5208cf8b52762f
Marin Todorov <marin@underplot.com>
2013-02-28 09:40:51 +0100

    + Starter Project

---
+++ Anagrams/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Anagrams
+//
+//  Created by Marin Todorov on 16/02/2013.
+//  Copyright (c) 2013 Underplot ltd. All rights reserved.
+//
+
+#import "config.h"
+#import "ViewController.h"
+
+@interface ViewController ()
+@end
+
+@implementation ViewController
+
+//setup the view and instantiate the game controller
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+//show tha game menu on app start
+-(void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+}
+
+@end
