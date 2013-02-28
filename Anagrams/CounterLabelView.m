810b6a01d23836e942577e294d757e6da1df558a
Marin Todorov <marin@underplot.com>
2013-02-28 16:43:07 +0100

    * counting functionality in the counter label

--- Anagrams/CounterLabelView.m
+++ Anagrams/CounterLabelView.m
@@ -43,6 +43,52 @@
     self.text = [NSString stringWithFormat:@" %i", self.value];
 }
 
+//count to a given value
+-(void)countTo:(int)to totalTime:(float)t
+{
+    //1 detect the time for the animation
+    delta = t/(abs(to-self.value)+1);
+    if (delta > 0.05) delta = 0.05;
+    
+    //2 set the end value
+	endValue = to;
+    
+    //3 cancel previous scheduled actions
+    [NSObject cancelPreviousPerformRequestsWithTarget:self];
+    
+    //4 detect which way counting goes
+    if (to-self.value>0) {
+        //count up
+        [self updateValueBy: @1];
+    } else {
+        //count down
+        [self updateValueBy: @-1];
+    }
+}
+
+//increment/decrement method
+-(void)updateValueBy:(NSNumber*)valueDelta
+{
+    //1 update the property
+    self.value += [valueDelta intValue];
+    
+    //2 check for reaching the end value
+	if ([valueDelta intValue]>0) {
+        if (self.value > endValue) {
+            self.value = endValue;
+            return;
+        }
+    } else {
+        if (self.value < endValue) {
+            self.value = endValue;
+            return;
+        }
+    }
+    
+    //3 if not - do it again
+    [self performSelector:@selector(updateValueBy:) withObject:valueDelta afterDelay:delta];
+}
+
 /*
 // Only override drawRect: if you perform custom drawing.
 // An empty implementation adversely affects performance during animation.
